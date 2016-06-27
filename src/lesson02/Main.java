b21652ee2807e034ff44f5c5a1da7b0854823e69
cerep89 <viorel.popa1989@gmail.com>
2016-06-27 05:44:04 +0300

    o clasa noua main in alt pachet

--- src/lesson02/Main.java
+++ src/lesson02/Main.java
@@ -1,8 +1,16 @@
 package lesson02;
 
+import ru.javawebinar.webapp.model.Link;
+
 /**
  * valeri
  * 27.06.2016.
  */
 public class Main {
+    public static void main(String[] args) {
+        Link link = new Link("Curs Java", "java.com");
+        Link link2 = link;
+        System.out.println(link.equals(link2));
+        System.out.println(link);
+    }
 }
