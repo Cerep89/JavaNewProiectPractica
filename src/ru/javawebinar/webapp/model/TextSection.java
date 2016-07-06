4bfcf588bbdd6c389d30fb12f0bf09a434ceeeb1
cerep89 <viorel.popa1989@gmail.com>
2016-07-06 10:58:06 +0300

    Creeted class TextSection extend Section

---
+++ src/ru/javawebinar/webapp/model/TextSection.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.webapp.model;
+
+/**
+ * Created by Viorel on 06.07.2016.
+ */
+public class TextSection extends  Section{
+   private String title;
+   private String comment;
+}
