599536cf50932d79049e95e0e4ec1bf98fc38234
cerep89 <viorel.popa1989@gmail.com>
2016-07-06 10:58:47 +0300

    Creeted class Organization

--- src/ru/javawebinar/webapp/model/Organization.java
+++ src/ru/javawebinar/webapp/model/Organization.java
@@ -1,8 +1,12 @@
 package ru.javawebinar.webapp.model;
 
+import java.util.List;
+
 /**
  * valeri
  * 27.06.2016.
  */
 public class Organization {
+    private Link link;
+    List<OrganizationPeriod> periods;
 }
