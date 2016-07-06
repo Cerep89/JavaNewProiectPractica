d5898fd15215e0650e3834d782495ef4abcc418e
cerep89 <viorel.popa1989@gmail.com>
2016-07-06 10:57:20 +0300

    Creeted class OrganizationSection extend Section

---
+++ src/ru/javawebinar/webapp/model/OrganizationSection.java
@@ -0,0 +1,12 @@
+package ru.javawebinar.webapp.model;
+
+
+import java.util.List;
+
+/**
+ * Created by Viorel on 06.07.2016.
+ */
+public class OrganizationSection extends  Section {
+private List<Organization> values;
+
+}
