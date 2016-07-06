e04f2435b15f73fbb78e46f2be32d9c983dcae67
cerep89 <viorel.popa1989@gmail.com>
2016-07-06 11:29:45 +0300

    Enhance SectionTape

--- src/ru/javawebinar/webapp/model/SectionType.java
+++ src/ru/javawebinar/webapp/model/SectionType.java
@@ -5,9 +5,20 @@ package ru.javawebinar.webapp.model;
  * 03.07.2016.
  */
 public enum SectionType {
-    OBJECTIVE,
-    ACHIEVEMENT,
-    QUALIFICATIONS,
-    EXPERIENCE,
-    EDUCATION
+    OBJECTIVE("Позицыя"),
+    ACHIEVEMENT("Достижения"),
+    QUALIFICATIONS("Квалификция"),
+    EXPERIENCE("Опыт Работы"),
+    EDUCATION("Оброзование");
+
+    private  String title;
+
+    SectionType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
 }
