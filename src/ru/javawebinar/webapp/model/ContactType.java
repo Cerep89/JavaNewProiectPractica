cef3fb2932e57342c1777c5ef3a63df2046b7499
cerep89 <viorel.popa1989@gmail.com>
2016-07-06 11:36:38 +0300

    Enhance ContactTape

--- src/ru/javawebinar/webapp/model/ContactType.java
+++ src/ru/javawebinar/webapp/model/ContactType.java
@@ -5,10 +5,20 @@ package ru.javawebinar.webapp.model;
  * 02.07.2016.
  */
 public enum ContactType {
-    PHONE,
-    MOBILE,
-    HOME_PHONE,
-    SKYPE,
-    EMAIL,
-    ICQ,
+    PHONE("Тел"),
+    MOBILE("Мобилиныи"),
+    HOME_PHONE("Домащнии"),
+    SKYPE("Skype"),
+    EMAIL("Почта"),
+    ICQ("Icq");
+
+    private  String title;
+
+    ContactType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
