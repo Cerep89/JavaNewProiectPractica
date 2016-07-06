6047eb18daf39236ab954934df49b3c72dde0cf8
cerep89 <viorel.popa1989@gmail.com>
2016-07-06 10:56:06 +0300

    Creeted class OrganizationPeriod which  have period for organization

---
+++ src/ru/javawebinar/webapp/model/OrganizationPeriod.java
@@ -0,0 +1,14 @@
+package ru.javawebinar.webapp.model;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Viorel on 06.07.2016.
+ */
+public class OrganizationPeriod {
+    List<Date> startDates;
+    List<Date> endDates;
+    List<String> positions;
+    List<String> content;
+}
